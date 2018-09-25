e9208263c63cd942869b941a194d7638d111d064
liza <lizajivnani@gmail.com>
2018-09-25 21:23:13 +0530

    first modification

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import *
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("this is homepage")
+
